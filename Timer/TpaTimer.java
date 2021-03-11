19b56b92ebc645f4d5695a7ca222c7226b281ded
Marcel Mikolas <marcel.mikolas@gmail.com>
2021-03-11 12:43:14 +0100

    Plugin

--- Timer/TpaTimer.java
+++ Timer/TpaTimer.java
@@ -4,13 +4,13 @@ import Main.Main;
 import org.bukkit.Bukkit;
 
 public class TpaTimer {
-    public static Integer CasTimer = Main.plugin.getConfig().getInt("messages.main.settings.PlayerPostdelay");
+
     public static void RunTpaTimer(){
         Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.plugin, new Runnable() {
             @Override
             public void run() {
                 TpaChecker.TpaPlayerChecker();
             }
-        }, 0, 20L * CasTimer);
+        }, 0, 20 * 3);
     }
 }
